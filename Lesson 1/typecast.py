df798032d7a80071e9f8ba79ba099fa434d3aeab
Zidan <muhammadzidankalaam@gmail.com>
2026-07-02 11:57:12 -0500

    Typecasting

---
+++ Lesson 1/typecast.py
@@ -0,0 +1,11 @@
+name = "Zidan"
+age = 9
+is_student = True
+weight = 30.10
+print("name",type(name))
+print("age",type (age))
+print("is_student",type (is_student))
+print("weight",type(weight))
+print("after typecasting")
+age = str(age)
+print("age",type (age))
