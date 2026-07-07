641620355a443fa87daa10406f6591c20a878910
Zidan <muhammadzidankalaam@gmail.com>
2026-07-07 11:46:50 -0500

    note

---
+++ operators.py
@@ -0,0 +1,9 @@
+tree1=58
+tree2=65
+tree3=89
+tree4=76
+tree5=54
+sum=tree1+tree2+tree3+tree4+tree5
+print("sum of all the 5 tree's:",sum)
+average=sum/5
+print("average of the 5 tree's is:",average)
