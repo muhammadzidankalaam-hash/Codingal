06d1b1ba3af8e8995e7b99a554a431dcd37dd58a
Zidan <muhammadzidankalaam@gmail.com>
2026-07-23 11:49:38 -0500

    Identity

---
+++ identity op.py
@@ -0,0 +1,22 @@
+x = 5
+if (type(x )is int):
+    print("true")
+
+else:
+
+    print("false")
+
+x = 7.7
+if (type(x)is not float):
+    print("true")
+
+else:
+    print("false")
+x = 9
+y = 9
+if ( x is y ):
+    print("x and y are same")
+y = 4
+if (x is not y):
+    print(" x and y are not same")
+
