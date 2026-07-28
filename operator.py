f2a5c6659d1f0cfb0a950bb2031017f8ff46815e
Zidan <muhammadzidankalaam@gmail.com>
2026-07-28 11:22:18 -0500

    Operators

---
+++ operator.py
@@ -0,0 +1,17 @@
+x = 3
+m = 9
+y = 9
+b = 2
+h = 1
+f = 0
+f = (m + y )*x/y;
+print(f)
+z = 0
+z = (m + h)*m / x ;
+print(z)
+name = "Alex"
+age = 0
+if name=="Alex"or name=="John"and age>=10:
+    print("hello world")
+else:
+    print("goodbye")
