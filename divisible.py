443b676e23ddab20b562021c810ffc2748d7d8d4
Zidan <muhammadzidankalaam@gmail.com>
2026-07-28 11:46:27 -0500

    Divisible

---
+++ divisible.py
@@ -0,0 +1,14 @@
+print("enter the number numerator")
+num =int(input("please enter the numerator"))
+print("enter the denominator")
+num1 =int(input("please enter the denominator"))
+if num %num1 ==0:
+    print(str(num)+"is divisible by"+ str (num1))
+else:
+    print(str(num)+"is not divisible by"+ str (num1))
+
+
+
+
+ 
+    
