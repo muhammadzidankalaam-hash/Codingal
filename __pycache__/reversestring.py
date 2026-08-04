e7bbd00697bf7b4d881d6bef9e2c4de25fc059e7
Zidan <muhammadzidankalaam@gmail.com>
2026-08-04 11:38:07 -0500

    Reverse

---
+++ __pycache__/reversestring.py
@@ -0,0 +1,8 @@
+string = input("enter the string you want to reverse :")
+string2 = ('')
+
+for i in string :
+    string2 = i + string2
+print("original string is",string)
+print("reverse string is ",string2)
+
