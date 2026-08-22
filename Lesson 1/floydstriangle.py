e1400e03656b51a046da315ff47cf2e474aa677e
Zidan <muhammadzidankalaam@gmail.com>
2026-08-22 10:44:52 -0500

    Floyd

---
+++ Lesson 1/floydstriangle.py
@@ -0,0 +1,8 @@
+n = int(input("please enter the total number of rows :"))
+number = 1 
+print("Floyd's Triangle")
+for i in range(1, n + 1):
+    for j in range(1, i + 1):
+        print(number, end=' ' )
+        number = number + 1
+    print()
--- Lesson 1/rightanglepattern.py
+++ Lesson 1/rightanglepattern.py
@@ -4,4 +4,5 @@ for i in range (n):
     for j in range (i+1):
      print("* ", end=" ")
     print()
+    
 
