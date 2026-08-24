34eeb6165b1ab44c60c439fbbecec78965ec7ef7
muhammadzidankalaam-hash <muhammadzidankalaam@gmail.com>
2026-08-23 19:31:12 -0500

    Update game.py

--- Lesson 1/game.py
+++ Lesson 1/game.py
@@ -1,6 +1,20 @@
-secret = 54 
-n =int("input"("What is your guess"))
-while ("after 5 attempts or player wins"):
-    if ("player incorrect")
+secret = 49
+attempts = 0 
+
+while attempts < 5:
+    n = int(input("What is your guess? "))
+    attempts +=1
+
+    if n == secret:
+        print("Hot")
+        break
+    elif n < secret:
+        print("cold")
     else:
+        print("warm")
+
+if attempts == 5 and n != secret:
+    print("You lost! The number was 49.")
+        
+
         
